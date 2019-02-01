036dddb5cb43326b39004b32b7d7a75d6fee41b2
HP <HP@DESKTOP-BO9O40E>
2019-02-01 14:04:22 +0200

    change x value

--- src/dalia/Homework.java
+++ src/dalia/Homework.java
@@ -5,6 +5,10 @@ public class Homework {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("The first Homework");
+		int x = 9 ; 
+		int y = 10 ; 
+		x = 18 ; 
+		
 
 	}
 
